341f8ed4f9cb2295eb8577b1d2032857e92e291f
NKragh <39466838+NKragh@users.noreply.github.com>
2018-11-08 12:04:22 +0100

    Fixed locations
    Now working

--- FranceVacances/Model/Searchbar.cs
+++ FranceVacances/Model/Searchbar.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace FranceVacances
+namespace FranceVacances.Model
 {
     class Searchbar
     {
--- FranceVacances/View/MainWindow.xaml.cs
+++ FranceVacances/View/MainWindow.xaml.cs
@@ -12,8 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FranceVacances.Model;
 
-namespace FranceVacances
+namespace FranceVacances.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
